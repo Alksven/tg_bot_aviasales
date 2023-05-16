65a662cbc254be2559ca047caa510d6137034aa7
Veniamin <alksven@gmail.com>
2023-05-16 18:01:18 +0300

    1

--- handlers/custom_handlers/search.py
+++ handlers/custom_handlers/search.py
@@ -1,11 +1,12 @@
-from aiogram import types
+from aiogram import types, filters
+from aiogram.dispatcher.filters import Text
 from loder import dp, bot
 from utils.code_city.iata_code import get_code_city
 from utils.get_tickets.get_tickets import get_tickets
 from aiogram.dispatcher import FSMContext
 from states.ticket_info import FlightInfo
-from keyboards.reply.calendar import start_date
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
+# from keyboards.reply.calendar import start_date
+from keyboards.reply.add_date import get_date
 
 
 
@@ -16,9 +17,10 @@ async def search(message: types.Message, state: FSMContext):
     await message.answer('Откуда?')
 
 
+
 @dp.message_handler(state=FlightInfo.from_city)
 async def get_city_from(message: types.Message, state: FSMContext):
-    code = get_code_city(message.text)
+    code = 'MOW' #get_code_city(message.text)
     await state.update_data(from_city=code)
     await state.set_state(FlightInfo.to_city)
     await message.answer('Куда?')
@@ -26,18 +28,21 @@ async def get_city_from(message: types.Message, state: FSMContext):
 
 @dp.message_handler(state=FlightInfo.to_city)
 async def get_city_to(message: types.Message, state: FSMContext):
-    code = get_code_city(message.text)
+    code = 'NOJ' #get_code_city(message.text)
     await state.update_data(to_city=code)
     await state.set_state(FlightInfo.from_date)
-    await message.answer('С какого числа ищем билеты?')
-    await start_date(message.chat.id)
+    kb = await get_date()
+    await message.answer(text='С какого числа ищем билеты?', reply_markup=kb)
+
 
 
 @dp.message_handler(state=FlightInfo.from_date)
 async def get_date_from(message: types.Message, state: FSMContext):
     await state.set_state(FlightInfo.to_date)
-    await message.answer('По какое число ищем билеты?')
-    await start_date(message.chat.id)
+    # kb = await get_date()
+    await message.answer('По какое число ищем билеты?', reply_markup=kb)
+    # await start_date(message.chat.id, cal_id=2)
+
 
 
 @dp.message_handler(state=FlightInfo.to_date)
--- utils/code_city/iata_code.py
+++ utils/code_city/iata_code.py
@@ -6,5 +6,6 @@ def get_code_city(city):
     """чтобы найти билет нужно получить IATA-код города. Данная функция получает этот код.,
     """
     url = requests.get(f'https://autocomplete.travelpayouts.com/places2?locale=en&types[]=airport&types[]=city&term={city}')
+    print(url)
     data = json.loads(url.text)
     return data[0]['code']
